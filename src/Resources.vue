b993d4bf2d6b61ded8797db19ca0a34dcd36bf04
Yerlin Matu <yerlinmatu@gmail.com>
2018-01-03 15:38:42 -0500

    Resource

--- src/Resources.vue
+++ src/Resources.vue
@@ -1,6 +1,6 @@
 <template lang="html">
 <div class="resources">
-  <h4>Resources for...</h4>
+  <h4 class="resources-title">Resources for...</h4>
   <div class="cardX">
      <div class="cardX-photo">
        <img src="./assets/Images/local_organization.png" alt="Photo" width="200">
@@ -26,8 +26,16 @@ export default {
 
 <style lang="scss">
   .resources {
-    background-color: #e1e1e1;
+    background-color: #e1e1e1 !important;
     height: 500px;
+    .resources-title {
+    //  position: absolute;
+      margin: 0 auto;
+      display: block;
+      text-align: center;
+  //    margin-top: 40px;
+
+    }
 
     .cardX {
       background-color: white;
@@ -36,17 +44,28 @@ export default {
       .cardX-title {
         width: 200px;
         height: 50px;
+        margin-top: -13px;
         background-color: #003c4d;
 
         strong {
           margin: 0 auto;
+          margin-top: 15px;
+          position: absolute;
+          display: block;
           text-align: center;
+          left: 50px;
           color: white;
         }
-        a {  color: black; }
+
       }
        .cardX-content {
-
+         a {
+           color: black;
+           margin:  0 auto;
+           margin: 10px, 10px, 10px, 10px;
+           text-align: center;
+           padding: 50px !important;
+          }
        }
     }
   }
